d13fabd4c1062f12c7c4763b714fa85adadb81f0
Fairus-24 <mufaza2408@gmail.com>
2025-05-17 17:49:12 +0700

    ok

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,8 +1,11 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  darkMode: "class",
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: 'class',
+  content: [
+    './index.html',                   
+    './src/**/*.{js,ts,jsx,tsx}',     
+  ],
   theme: {
     extend: {
       borderRadius: {
