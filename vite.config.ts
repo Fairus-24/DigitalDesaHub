9c5dfaec36c3dece394079afef9c999513858620
Fairus-24 <mufaza2408@gmail.com>
2025-05-18 18:21:39 +0700

    bismillah mugo tepak

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
+
 export default defineConfig({
   root: 'client',  // arahkan ke folder client
   resolve: {
@@ -14,5 +15,6 @@ export default defineConfig({
     outDir: '../dist',    // hasil build di root/dist
     emptyOutDir: true
   },
-  plugins: [react()]
+  plugins: [react(),]
+  
 });
