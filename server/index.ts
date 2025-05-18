ce4fb37d6b3ffa3a04662c02bdd79afbc1593495
Fairus-24 <mufaza2408@gmail.com>
2025-05-19 05:22:12 +0700

    lol

--- server/index.ts
+++ server/index.ts
@@ -65,7 +65,7 @@ app.use((req, res, next) => {
   }
 
   // ALWAYS serve the app on port from env (for Railway)
-  const port = process.env.PORT || 5000;
+  const port = Number(process.env.PORT) || 5000;
   server.listen(port, '0.0.0.0', () => {
     log(`serving on port ${port}`);
   });
