8b03347c4de7413bacb701c382b17c2d60c22461
Fairus-24 <mufaza2408@gmail.com>
2025-05-18 18:28:31 +0700

    railway

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
 
 
 export default defineConfig({
@@ -12,7 +13,8 @@ export default defineConfig({
     }
   },
   build: {
-    outDir: '../dist',    // hasil build di root/dist
+   // outDir: '../dist', 
+    outDir: path.resolve(__dirname, '../server/client'),    // hasil build di root/dist
     emptyOutDir: true
   },
   plugins: [react(),]
